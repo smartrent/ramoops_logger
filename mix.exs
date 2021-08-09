02b35c76e8bdc5bbec064076310a48d6e62a68d3
Grace Yanagida <gracetheprogrammer@gmail.com>
2021-08-09 10:57:36 -0400

    CI: enable credo

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule RamoopsLogger.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: [:test, :dev], runtime: false}
     ]
   end
