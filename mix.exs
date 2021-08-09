c96821ba77ad67a72409bb1a70a0520eb18a0540
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-09 18:26:16 -0400

    CI: Bump tested Elixir versions; drop 1.7

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule RamoopsLogger.MixProject do
     [
       app: :ramoops_logger,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
