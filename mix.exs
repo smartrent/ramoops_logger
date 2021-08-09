c31e81f74c8d2faa809d0d180f1d68ee4bafb89c
Frank Hunleth <fhunleth@troodon-software.com>
2021-08-09 15:18:38 -0400

    Bump all deps to latest

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule RamoopsLogger.MixProject do
     [
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:test, :dev], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:test, :dev], runtime: false}
     ]
   end
 
